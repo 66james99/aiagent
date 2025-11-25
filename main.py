3427f5e35d8a5abc425fdd156714ed57c87328cd
66james99 <j.e.hammick@gmail.com>
2025-11-25 09:00:34 +0000

    update to use messages

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ import sys
 import os
 from google import genai
 from dotenv import load_dotenv
+from google.genai import types
 
 
 def main():
@@ -15,13 +16,17 @@ def main():
         print('Example: python main.py "How do I build a calculator app?"')
         sys.exit(1)
     user_prompt = " ".join(args)
+ 
+    messages = [
+        types.Content(role="user", parts=[types.Part(text=user_prompt)]),
+    ]
 
     api_key = os.environ.get("GEMINI_API_KEY")
     client = genai.Client(api_key=api_key)
 
     response = client.models.generate_content(
         model="gemini-2.0-flash-001",
-        contents=user_prompt,
+        contents=messages,
     )
     print("Prompt tokens:", response.usage_metadata.prompt_token_count)
     print("Response tokens:", response.usage_metadata.candidates_token_count)
