c559ecf33bd3b9d423cb2f8efb784a687f248c30
66james99 <j.e.hammick@gmail.com>
2025-11-25 12:52:31 +0000

    updated to match Boot DEV version of various files

--- tests.py
+++ tests.py
@@ -1,33 +1,21 @@
-from functions.run_python_file import run_python_file
+from functions.run_python import run_python_file
 
 
 def test():
     result = run_python_file("calculator", "main.py")
-    print("Result of trying to run main.py:")
-    print(result)
-    print("")
-
-    result = run_python_file("calculator", "main.py", ["3 + 5"])
-    print("Result of trying to run main.py 3 + 5:")
     print(result)
 
     result = run_python_file("calculator", "tests.py")
-    print("Result of trying to run tests.py:")
     print(result)
 
     result = run_python_file("calculator", "../main.py")
-    print("Result of trying to run ../main.py:")
     print(result)
 
     result = run_python_file("calculator", "nonexistent.py")
-    print("Result of trying to run nonexistent.py:")
     print(result)
 
     result = run_python_file("calculator", "lorem.txt")
-    print("Result of trying to run lorem.txt:")
     print(result)
-
-
 
 
 if __name__ == "__main__":
